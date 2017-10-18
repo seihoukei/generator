993a7a7665afab13199b723a7b582e8dd5fe3f52
seihoukei <31934643+seihoukei@users.noreply.github.com>
2017-10-18 21:41:41 +0300

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -27,24 +27,25 @@ let cacheFirst = new Set(cacheToNetwork)
 
 self.addEventListener('install', event => {
 	event.waitUntil(
-		caches.open(cacheName).then(cache => {
-			return cache.addAll([...cacheToNetwork, ...networkToCache])
-		})
+		caches.open(cacheName).then(cache => cache.addAll([...cacheToNetwork, ...networkToCache]))
 	)
 })
 
 self.addEventListener('fetch', event => {
 	if (cacheFirst.has(event.request.url)) {
 		event.respondWith(
-			caches.match(event.request).then(response => {
-				return response || fetch(event.request)
-			})
+			caches.open(cacheName)
+				.then(cache => cache.match(event.request))
+				.then(response => (response || fetch(event.request)))
 		)
 	} else {
 		event.respondWith(
-			fetch(event.request).catch(() =>{
-				return caches.match(event.request)
-			})
+			caches.open(cacheName)
+				.then(cache => fetch(event.request)
+					.then(response => cache.put(event.request, response.clone())
+						.then(() => response))
+					.catch(() => cache.match(event.request))
+			)
 		)
 	}
 })
